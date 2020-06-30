e9e8f3c741b6f05797df10e1b171385a27de153e
entin.wei <entin.wei@rakuten.com>
2020-06-30 13:04:59 +0800

    Updates

---
+++ precache-manifest.2b69169a85fc797e591e7f02642d43a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831a98f2f3f50eb9475bf28b0a658036",
+    "url": "/song-search-xstate/index.html"
+  },
+  {
+    "revision": "bab53c0a93e5be98cd61",
+    "url": "/song-search-xstate/static/css/main.18483c3f.chunk.css"
+  },
+  {
+    "revision": "e013e0cb973e4134e938",
+    "url": "/song-search-xstate/static/js/2.f0648cef.chunk.js"
+  },
+  {
+    "revision": "49eb8843f2ec5480c31ee4f7ce295c84",
+    "url": "/song-search-xstate/static/js/2.f0648cef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bab53c0a93e5be98cd61",
+    "url": "/song-search-xstate/static/js/main.88bf6cc7.chunk.js"
+  },
+  {
+    "revision": "5f40da4e6c46e24abb27",
+    "url": "/song-search-xstate/static/js/runtime-main.02628861.js"
+  },
+  {
+    "revision": "4f744e8239d2552ad4ddf0b89b75a046",
+    "url": "/song-search-xstate/static/media/cross.4f744e82.svg"
+  },
+  {
+    "revision": "0f51771c9da2d2678d0c8c22a4867ab0",
+    "url": "/song-search-xstate/static/media/loading.0f51771c.svg"
+  },
+  {
+    "revision": "fa72ba04bd8bb65f81e0bef1804b0ace",
+    "url": "/song-search-xstate/static/media/search.fa72ba04.svg"
+  }
+]);
